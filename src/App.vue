639c8a4d61d384a653232f34e0cf21bf2069353d
hiro-551010 <hiro551010@gmail.com>
2021-06-22 11:00:35 +0900

    firstcommit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="main">
+    <button @click="show = !show">切り替え</button>
+    <!-- nameが無い場合はvが入ってくる -->
+    <transition name="fade">
+      <p v-if="show">hello</p>
+    </transition>
+    <transition name="slide">
+      <div v-if="show">
+        <p>bye</p>
+        <p>hello</p>
+        <p>hello</p>
+      </div>
+    </transition>
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return {
+      show: true,
+    };
+  }
+};
+</script>
+
+<style scoped>
+.main {
+  width: 70%;
+  margin: auto;
+  padding-top: 5rem;
+  text-align: center;
+}
+
+/* transitionクラス */
+.fade-enter {
+  /* 現れる時の最初の状態 */
+  opacity: 0;
+}
+.fade-enter-active {
+  /* 現れる時のトランジションの状態 */
+  transition: opacity 0.5s;
+}
+.fade-enter-to {
+  /* 現れる時の最後の状態 */
+  opacity: 1;
+}
+.fade-leave {
+  /* 消える時の最初の状態 */
+  opacity: 1;
+}
+.fade-leave-active {
+  /* 消える時のトランジションの状態 */
+  transition: opacity 0.5s;
+}
+.fade-leave-to {
+  /* 消える時の最後の状態 */
+  opacity: 0;
+}
+
+.slide-enter-active {
+  animation: slide-in 0.5s;
+}
+.slide-leave-active {
+  animation: slide-in 0.5s reverse;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateX(100px);
+  }
+  to {
+    transform: translateX(0)
+  }
+}
+</style>
